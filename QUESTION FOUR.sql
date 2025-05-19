a27f4c9ce6b58051dd63e063402844cf879cb577
Naluwemba Latifah <muhammadlatifah26@gmail.com>
2025-05-19 12:43:50 +0300

    Add files via upload

---
+++ QUESTION FOUR.sql
@@ -0,0 +1,48 @@
+-- QUESTION FOUR
+
+-- CREATE DATABASE EXAM;
+-- CREATE table student(
+-- code varchar (20),
+-- stud_id int,
+-- Name varchar (30));student
+
+-- insert into student(code,stud_id,Name)
+-- values('IMIS' ,'001' , 'Damaris'),
+--       ('BIT' , '002' , 'Okal'),
+--       ('BIT' , '003' , 'Kahuria'),
+--       ('CIT' , '004' , 'Mwangi');
+      
+-- create table Course(
+-- Code varchar (30),
+-- Title varchar(40));
+
+
+-- insert into Course(Code,Title)
+-- values('IMIS' , 'Info. Systems'),
+-- 	   ('BIT' ,'Bachelor of IT'),
+--        ('CIT' , 'Cert in IT'),
+--        ('DIT' , 'Dip in IT')
+
+-- update student 
+-- set Name = 'Latifah'
+-- WHERE code = 'BIT'
+
+-- Delete from student 
+-- where code = 'BIT'
+
+-- select *
+-- from student
+
+-- select * 
+-- from Course
+
+
+
+
+
+
+
+
+
+
+
---
+++ QUESTION ONE.sql
@@ -0,0 +1,39 @@
+-- create table finance(
+-- student_name varchar (30),
+-- student_id int,
+-- year varchar (35),
+-- tution_fee int)
+
+-- insert into finance
+-- values('jane' , '235' , 'Y1S2' , '30000'),
+--       ('nicholas' , '26' , 'Y1S1' , '40000'),
+--       ('cyrus' , '1' , 'Y2S1' , '34000'),
+--       ('neema' , '2' , 'Y2S1' , '23000'),
+--       ('caroline' , '3' , 'Y1S1' , '20000'),
+--       ('judith' , '29' , 'Y4S2' , '35000'),
+--       ('grace' , '127' , 'Y1S2' , '28000')
+
+-- select *
+-- from finance
+-- where year = 'Y1S1'
+
+
+
+
+-- select student_name , student_id , year, tution_fee
+-- from finance
+-- where tution_fee >= 30000
+
+-- select*
+-- from finance 
+-- add finance  'Dan' ,20/4/2001, 'Y1S2', 0723456123
+
+
+-- select student_name 
+-- from finance
+-- add 'nelson' where student_id =26
+
+
+SELECT *
+FROM finance
+
